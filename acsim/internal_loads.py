7e8a0f891cae7dfcc761a022dd00469b3b8be556
hdahhaoui <d_hachimi@yahoo.com>
2025-04-19 00:10:07 +0100

    Create internal_loads.py

---
+++ acsim/internal_loads.py
@@ -0,0 +1,11 @@
+# Charges internes (apports de chaleur internes) en watts selon le niveau
+# (incluent par exemple les personnes, éclairages, équipements électriques)
+internal_gains = {
+    "Faible": 500.0,    # Faibles apports internes (petite occupation, équipements économes)
+    "Moyen": 1000.0,   # Apports moyens
+    "Élevé": 1500.0    # Apports élevés (forte occupation, appareils nombreux)
+}
+
+def charges_internes(niveau_charges):
+    """Retourne la valeur des apports de chaleur internes (W) pour le niveau choisi."""
+    return internal_gains[niveau_charges]
