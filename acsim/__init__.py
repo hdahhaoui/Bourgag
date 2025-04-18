c487ccb0a36efd512bd132f5984198d34726d985
hdahhaoui <d_hachimi@yahoo.com>
2025-04-19 00:05:39 +0100

    Create __init__.py

---
+++ acsim/__init__.py
@@ -0,0 +1,3 @@
+# Initialisation du package acsim
+# On importe les sous-modules pour simplifier leur accès à l'utilisation (e.g. acsim.weather)
+from . import weather, envelope, ventilation, internal_loads, ac_model, thermal_model, scenario, ai_analysis
